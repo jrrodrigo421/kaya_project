e46a4a59c6efb62965eaa23f5857cdf0515856d2
jrrodrigo42 <rjr89000@gmail.com>
2025-03-21 18:38:38 -0300

    feat: redirect root URL to /medicos/

--- kaya_project/urls.py
+++ kaya_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('medicos/', include('doctors.urls')),
+]
+urlpatterns += [
+    path('', lambda request: HttpResponseRedirect('/medicos/')),
 ]
